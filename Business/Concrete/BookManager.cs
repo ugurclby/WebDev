155f775699fe5f69e83f49c43c0a9bf104d2be4e
Uğur Çalbay <ugur.calbay@univera.local>
2020-05-03 18:34:20 +0300

    Add project files.

---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        private IBookDal _bookDal;
+
+        public BookManager (IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public void Add(Book book)
+        {
+            _bookDal.Add(book);
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetList().ToList();
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+            return _bookDal.GetList(x => x.AuthorId == id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(x => x.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
