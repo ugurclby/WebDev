3457dc235e69c8b5bd0dbc7bf2dd8e60a85bd592
uğur çalbay <ugurcalbay@galatasaray.net>
2020-05-03 21:15:11 +0300

    Startup düzenleme

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +29,8 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IBookService, BookManager>();
+            services.AddSingleton<IBookDal, EfBookDal>();
             services.AddControllers();
         }
 
